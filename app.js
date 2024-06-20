7c6f7ae8bb046fa84ca49f01d08cadb3d02b2600
JuliaGayotto <juliagayotto@gmail.com>
2024-06-20 00:31:38 -0300

    adicao do crud

--- app.js
+++ app.js
@@ -22,6 +22,43 @@ app.post('/usuario/cadastro', async (req, res) => {
     res.send(resultado);
 });
 
+app.get('/usuario/selecionar/:value', async (req, res) => {
+  var usuario = new Usuario();
+  const { value } = req.params;
+
+  try {
+      var resultado = await usuario.selecionar(value);
+  } catch (error) {
+      console.error("Erro ao selecionar dados do usuário!")
+  }
+  res.send(resultado);
+});
+
+app.put('/usuario/atualizar', async (req, res) => {
+  const userData = req.body;
+  const usuario = new Usuario();
+  
+  try {
+    const userId = await usuario.atualizar(userData);
+    res.status(200).json({ data: userId });
+  } catch (error) {
+    res.status(500).send({ error: 'Erro ao adicionar informações'});
+  }
+});
+
+app.put('/usuario/atualizar/completo', async (req, res) => {
+  const userData = req.body;
+  const usuario = new Usuario();
+  
+  try {
+    const id = await usuario.atualizarCompleto(userData);
+    res.status(200).json({ mensagem: "usuario atualizado"});
+  } catch (error) {
+    res.status(500).send({ error: 'Erro ao adicionar informações'});
+  }
+});
+
+
 app.delete('/usuario/:uid', async (req, res) => {
     const { uid } = req.params;
     const usuario = new Usuario();
@@ -32,7 +69,7 @@ app.delete('/usuario/:uid', async (req, res) => {
     } catch (error) {
       res.status(500).send({ error: 'Erro ao excluir e anonimizar usuário', details: error.message });
     }
-  });
+});
 
 
 
--- models/usuario.js
+++ models/usuario.js
@@ -9,7 +9,7 @@ if (admin.apps.length === 0) {
 
 class Usuario {
   async cadastro(data) {
-    const { nome, email } = data;
+    const { nome, email} = data;
 
     const db = admin.firestore();
 
@@ -18,12 +18,102 @@ class Usuario {
         nome: nome,
         email: email
       });
-      console.log('Documento escrito com ID:', docRef.id);
+      res.status(200).json({ userId: docRef.uid });
     } catch (error) {
       console.error('Erro ao adicionar documento:', error.message);
     }
   };
 
+  async selecionar(email) {
+    const db = admin.firestore();
+    try {
+      const snapshot = await db.collection('users').where('email', '==', email).get();
+  
+      if (snapshot.empty) {
+        return null; 
+      }
+  
+      return snapshot.docs[0].data();
+  
+    } catch (error) {
+      console.error("Erro selecionando usuario: ", error.message);
+      throw error; 
+    }
+  }
+  
+
+  async atualizar(userData) {
+    const { email, dataNascimento, telefone, endereco } = userData;
+
+    const db = admin.firestore();
+
+    try {
+    
+    const snapshot = await db.collection('users').where('email', '==', email).get();
+
+    if (snapshot.empty) {
+      return null;
+    }
+
+    let userId;
+    snapshot.forEach(doc => {
+      userId = doc.id;
+    });
+
+    const userRef = db.collection('users').doc(userId);
+
+    await userRef.update({
+        dataNascimento: dataNascimento,
+        telefone: telefone,
+        endereco: endereco
+      });
+
+    console.log(`Usuário atualizado`);
+    return userId
+    } catch (error) {
+      console.error("Erro atualizando usuario: ", error.message);
+    }
+  }  
+
+
+  async atualizarCompleto(userData) {
+    const { userId, nome, email, emailNovo, dataNascimento, telefone, endereco } = userData;
+
+    const db = admin.firestore();
+
+    try {
+    
+    const snapshot = await db.collection('users').where('email', '==', email).get();
+
+    if (snapshot.empty) {
+      return null;
+    }
+
+    let id;
+    snapshot.forEach(doc => {
+      id = doc.id;
+    });
+
+    const userRef = db.collection('users').doc(id);
+
+    await userRef.update({
+        nome: nome,
+        email: emailNovo,
+        dataNascimento: dataNascimento,
+        telefone: telefone,
+        endereco: endereco
+      });
+
+    await admin.auth().updateUser(userId, {
+        email: emailNovo
+      });
+
+
+    return id
+    } catch (error) {
+      console.error("Erro atualizando usuario: ", error.message);
+    }
+  }  
   async addUserToExclusionList(userId) {
     const db = admin.firestore();
     
